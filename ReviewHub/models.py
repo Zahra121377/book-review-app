4058d6f5d71e9ac1464f061b6fb39186f17ffe67
Zahra121377 <afkhami121377@gmail.com>
2023-12-22 03:33:41 +0330

    Solve pre-commit issues

--- ReviewHub/models.py
+++ ReviewHub/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 
+
 class Book(models.Model):
-    title = models.CharField(max_length = 200 , blank = False)
-    author = models.CharField(max_length = 70)
+    title = models.CharField(max_length=200, blank=False)
+    author = models.CharField(max_length=70)
     pub_year = models.PositiveSmallIntegerField(blank=True, null=True)
     GENRE_LIST = [
-        ('Fiction', 'Fiction'),
-        ('Novel', 'Novel'),
-        ('Narrative', 'Narrative'),
-        ('Mystery','Mystery'),
-        ('Sci-Fi', 'Science Fiction'),
-        ('Fantasy', 'Fantasy'),
-        ('Thriller','Thriller'),
-        ('History','History'),
-        ('Memoir','Memoir'),
-        ('Humor','Humor')
+        ("Fiction", "Fiction"),
+        ("Novel", "Novel"),
+        ("Narrative", "Narrative"),
+        ("Mystery", "Mystery"),
+        ("Sci-Fi", "Science Fiction"),
+        ("Fantasy", "Fantasy"),
+        ("Thriller", "Thriller"),
+        ("History", "History"),
+        ("Memoir", "Memoir"),
+        ("Humor", "Humor"),
     ]
-    genre = models.CharField(max_length = 25, choices = GENRE_LIST)
-    
+    genre = models.CharField(max_length=25, choices=GENRE_LIST)
+
     def __str__(self):
         return self.title
