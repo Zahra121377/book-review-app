fe1fe883d3ff841b5d9cce6482a688507153d302
Zahra121377 <afkhami121377@gmail.com>
2023-12-22 15:03:40 +0330

    Define Review model

--- ReviewHub/models.py
+++ ReviewHub/models.py
@@ -21,3 +21,16 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    context = models.TextField(max_length = 1000)
+    STARS = [
+        ("1", "1"),
+        ("2", "2"),
+        ("3", "3"),
+        ("4", "4"),
+        ("5", "5"),
+    ]
+    rating = models.CharField(max_length = 1, choices = STARS)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
