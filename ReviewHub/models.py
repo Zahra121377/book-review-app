55122586dff431841f9b1dd6ada358719043d879
Zahra121377 <afkhami121377@gmail.com>
2023-12-22 03:31:53 +0330

    Create Book model

--- ReviewHub/models.py
+++ ReviewHub/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length = 200 , blank = False)
+    author = models.CharField(max_length = 70)
+    pub_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    GENRE_LIST = [
+        ('Fiction', 'Fiction'),
+        ('Novel', 'Novel'),
+        ('Narrative', 'Narrative'),
+        ('Mystery','Mystery'),
+        ('Sci-Fi', 'Science Fiction'),
+        ('Fantasy', 'Fantasy'),
+        ('Thriller','Thriller'),
+        ('History','History'),
+        ('Memoir','Memoir'),
+        ('Humor','Humor')
+    ]
+    genre = models.CharField(max_length = 25, choices = GENRE_LIST)
+    
+    def __str__(self):
+        return self.title
