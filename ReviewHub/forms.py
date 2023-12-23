942e8d932351c56c0ef82a1beff36c4dd2269a68
Zahra121377 <afkhami121377@gmail.com>
2023-12-24 00:29:03 +0330

    Create basic review form

--- ReviewHub/forms.py
+++ ReviewHub/forms.py
@@ -1,17 +1,25 @@
 from django import forms
+
 from .models import Review
+
+
 class ReviewForm(forms.ModelForm):
     context = forms.CharField(
-        required= True, 
+        required=True,
         label="Review",
-        widget=forms.Textarea(attrs={"class": "form-control", "placeholder": "Write your review here"}),
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Write your review here",
+            }
+        ),
     )
     rating = forms.ChoiceField(
         choices=Review.STARS,
         label="Rating",
         widget=forms.RadioSelect,
     )
+
     class Meta:
         model = Review
-        fields = ['rating', 'context']
-        
+        fields = ["context", "rating"]
