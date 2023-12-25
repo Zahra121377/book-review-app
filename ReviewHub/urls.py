3f5439aaa78532340df256eec66984315be0b9e5
Zahra121377 <afkhami121377@gmail.com>
2023-12-25 16:48:38 +0330

    Fix add review part

--- ReviewHub/urls.py
+++ ReviewHub/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("add_review/<int:book_id>/", views.add_review, name="add_review"),
+    path(
+        "add_review/<int:book_id>/",
+        views.CreateReviewView.as_view(),
+        name="add_review",
+    ),
     path("book_list", views.BookListView.as_view(), name="book_list"),
 ]
--- ReviewHub/views.py
+++ ReviewHub/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
-from django.shortcuts import redirect, render
+
+
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import ListView
+from django.views.generic.edit import FormView
 
 from ReviewHub.forms import ReviewForm
 from ReviewHub.models import Book
@@ -19,19 +22,39 @@ def index(request):
     return render(request, "ReviewHub/index.html")
 
 
-def add_review(request, book_id):
-    book = Book.objects.get(pk=book_id)
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.book = book
-            review.save()
-            return redirect("book_detail", book_id=book_id)
-    else:
-        form = ReviewForm()
+# def add_review(request, book_id):
+#     book = Book.objects.get(pk=book_id)
+#     if request.method == "POST":
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             review = form.save(commit=False)
+#             review.book = book
+#             review.save()
+#             return redirect("book_detail", book_id=book.id)
+#     else:
+#         form = ReviewForm()
 
-    return render(request, "ReviewHub/add_review.html", {"form": form})
+#     return render(request, "ReviewHub/add_review.html", {"form": form})
+
+
+class CreateReviewView(FormView):
+    template_name = "ReviewHub/add_review.html"
+    form_class = ReviewForm
+
+    def form_valid(self, form):
+        book = get_object_or_404(Book, id=self.kwargs["book_id"])
+        review = form.save(commit=False)
+        review.book = book
+        review.user = self.request.user
+        review.save()
+        return redirect(
+            "book_list"
+        )  # Adjust the URL name based on your URL patterns
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["book"] = get_object_or_404(Book, id=self.kwargs["book_id"])
+        return context
 
 
 class BookListView(ListView):
